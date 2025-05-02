406467868ec936283e18ac53a9c2d5540432782b
rumoso <ing.rubenmoreno@gmail.com>
2025-05-01 21:16:44 -0700

    Se suben ajustes

--- ClaseUnica/ClaseUnica/DBMS_Unico.cs
+++ ClaseUnica/ClaseUnica/DBMS_Unico.cs
@@ -28,7 +28,7 @@ namespace UnicaSQL
                     break;
 
                 case "MySQL":
-                    sConexion = string.Format("server={0}; database={1}; Uid={2}; pwd={3}; AllowUserVariables=True;", sServer, sBD, sUsuario, sPassword);
+                    sConexion = string.Format("server={0}; database={1}; Uid={2}; pwd={3}; AllowUserVariables=True; port={4};", sServer, sBD, sUsuario, sPassword, sPuerto);
                     MySQL = new DBMS_MySQL(sConexion);
                     break;
 
